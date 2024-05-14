328c9f1a82573a3a8ebd5eeeefcf5926c12c6c66
Susanna Di Vita <susanna.divita.2@gmail.com>
2024-05-14 14:31:35 +0000

    distance 1

--- src/tsne/tsne_5.c
+++ src/tsne/tsne_5.c
@@ -121,13 +121,15 @@ static void calculate_p(double *X, double *P, int n, int dx, double perplexity)
 
     for (int i = 0; i < n; i++)
     {
+        int q = i * n;
         for (int j = i+1; j < n; j++)
         {
-            double tmp = (P[i * n + j] + P[j * n + i]) / (2*n);
+            int w = j * n;
+            double tmp = (P[q + j] + P[w + i]) / (2*n);
             tmp = fmax(tmp, 1e-12);
 
-            P[i * n + j] = tmp;
-            P[j * n + i] = tmp;
+            P[q + j] = tmp;
+            P[w + i] = tmp;
         }
     }
 
