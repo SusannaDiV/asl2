58998c0f474987aa397356a366432c9f1ac3cade
Susanna Di Vita <susanna.divita.2@gmail.com>
2024-05-14 14:40:56 +0000

    distance 1

--- src/tsne/tsne_5.c
+++ src/tsne/tsne_5.c
@@ -4,7 +4,6 @@
 #include <memory.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <immintrin.h>
 
 #include "tsne.h"
 
@@ -12,8 +11,6 @@ static double h_beta(double *D, double *P, int n, int d, double beta, int exclud
 {
     double sumP = 0.0;
     double sumDP = 0.0;
-    
-    double inv_sumP = 0.0;
     for (int i = 0; i < n; i++)
     {
         if (i == exclude_index)
@@ -27,17 +24,14 @@ static double h_beta(double *D, double *P, int n, int d, double beta, int exclud
         sumDP += D[i] * P[i];
     }
 
-    if (sumP != 0.0) {
-        inv_sumP = 1.0 / sumP;
-    }
-
     for (int i = 0; i < n; i++)
     {
-        P[i] *= inv_sumP; 
+        P[i] /= sumP; // WTF do this every time? We need only the final P
     }
 
-    return log(sumP) + beta * sumDP * inv_sumP;
+    return log(sumP) + beta * sumDP / sumP;
 }
+
 
 // W(n,dim_y) = 3n^2*dim_y
 static void distance_squared(double *X, double *D, int n, int d)
@@ -45,18 +39,18 @@ static void distance_squared(double *X, double *D, int n, int d)
     for (int i = 0; i < n; i++)
     {
         int q = i * d;
-        int w = i * n;
+        int m = i * n;
         for (int j = 0; j < n; j++)
         {
-            int t = j * d;
+            int w = j * d;
             double dist = 0.0;
             double diff;
             for (int k = 0; k < d; k++)
             {
-                diff = X[q + k] - X[t + k];
+                diff = X[q + k] - X[w + k];
                 dist += diff * diff;
             }
-            D[w + j] = dist;
+            D[m + j] = dist;
         }
     }
 }
@@ -67,15 +61,19 @@ static void x2p(double *X, double *P, int n, int d, double perplexity)
     double *beta = (double *) malloc(sizeof(double) * n);
 
     distance_squared(X, D, n, d);
-    double tol = 1e-5;
+
     double logU = log(perplexity);
 
     for (int i = 0; i < n; i++)
     {
         beta[i] = 1.0;
-        double h = h_beta(D + i * n, P + i * n, n, d, beta[i], i);
+        int q = i * n;
+        double* w = q + D;
+        double* m = q + P;
+        double h = h_beta(w, m, n, d, beta[i], i);
         double Hdiff = h - logU;
         int tries = 0;
+        double tol = 1e-5;
 
         double betamin = -INFINITY;
         double betamax = INFINITY;
@@ -91,7 +89,7 @@ static void x2p(double *X, double *P, int n, int d, double perplexity)
                 }
                 else
                 {
-                    beta[i] = (beta[i] + betamax) / 2.;
+                    beta[i] = (beta[i] + betamax) * 0.5;
                 }
             }
             else
@@ -99,15 +97,15 @@ static void x2p(double *X, double *P, int n, int d, double perplexity)
                 betamax = beta[i];
                 if (betamin == INFINITY || betamin == -INFINITY)
                 {
-                    beta[i] = beta[i] / 2.;
+                    beta[i] = beta[i] * 0.5;
                 }
                 else
                 {
-                    beta[i] = (beta[i] + betamin) / 2.;
+                    beta[i] = (beta[i] + betamin) * 0.5;
                 }
             }
 
-            h = h_beta(D + i * n, P + i * n, n, d, beta[i], i);
+            h = h_beta(w, m, n, d, beta[i], i);
             Hdiff = h - logU;
             tries += 1;
         }
@@ -120,76 +118,85 @@ static void x2p(double *X, double *P, int n, int d, double perplexity)
 static void calculate_p(double *X, double *P, int n, int dx, double perplexity)
 {
     x2p(X, P, n, dx, perplexity);
-    int nn = 2 * n;
-    double inv_nn = 1.0 / nn;
 
     for (int i = 0; i < n; i++)
     {
-        int q = i * n;
-        for (int j = i + 1; j < n; j++)
+        for (int j = i+1; j < n; j++)
         {
-            int w = j * n;
-            int wi = w + i;
-            int qj = q + j;
+            double tmp = (P[i * n + j] + P[j * n + i]) / (2*n);
+            tmp = fmax(tmp, 1e-12);
 
-            double tmp_sum = P[qj] + P[wi];
-            double tmp = tmp_sum * inv_nn;
+            P[i * n + j] = tmp;
+            P[j * n + i] = tmp;
+        }
+    }
 
-            P[qj] = tmp;
-            P[wi] = tmp;
+     // Reorganize 
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            P[k] = P[i*n + j];
+            k++;
         }
     }
 }
 
-
-// W(n) = 18n^2
+// W(n) = 12n^2
 static void compute_gradient(double *P, double *Q, double *Y, double *dY, int n, int dy)
 {
     // 1. Compute the sum of Q
     double Qsum = 0.0;
     for (int i = 0; i < n; i++)
     {
-        int q = 2 * i;
         for (int j = i+1; j < n; j++)
         {
-            int w = 2 * j;
-            double y_diff0 = Y[q] - Y[w];
-            double y_diff1 = Y[q + 1] - Y[w + 1];
-            double a = y_diff0 * y_diff0;
-            double b = y_diff1 * y_diff1;
-            double y_dist = a + b;
-            double inv = 1 + y_dist;
-            Qsum += 2 / inv;
+            double y_diff0 = Y[2*i + 0] - Y[2*j + 0];
+            double y_diff1 = Y[2*i + 1] - Y[2*j + 1];
+            double y_dist =  y_diff0 * y_diff0 + y_diff1 * y_diff1;
+
+            Qsum += 2 / (1 + y_dist);
         }
     }
     double Qsum_inv = 1 / Qsum;
 
+    memset(dY, 0, 2*n*sizeof(double));
+
     // 2. Update the gradients
+    int P_idx = 0;
     for (int i = 0; i < n; i++)
     {
-        int q = 2 * i;
-        int r = i * dy;
-        dY[r] = 0.0;
-        dY[r + 1] = 0.0;
-        for (int j = 0; j < n; j++)
-        {
-            int w = 2 * j;
-            double y_diff0 = Y[q] - Y[w];
-            double y_diff1 = Y[q + 1] - Y[w + 1];
-            double a = y_diff0 * y_diff0;
-            double b = y_diff1 * y_diff1;
-            double y_dist = a + b;
-            double inv = 1 + y_dist;
-            double y_dist_p1_inv = 1 / inv;
+        double dYi0 = dY[2*i + 0];
+        double dYi1 = dY[2*i + 1];
 
-            double P_ij = P[i * n + j];
-            double Q_ij = i != j ? Qsum_inv * y_dist_p1_inv : 0.0;
+        for (int j = i+1; j < n; j++)
+        {
+            double y_diff0 = Y[2*i + 0] - Y[2*j + 0];
+            double y_diff1 = Y[2*i + 1] - Y[2*j + 1];
+            double y_dist =  y_diff0 * y_diff0 + y_diff1 * y_diff1;
+            double y_dist_p1_inv = 1 / (1 + y_dist);
+
+            double P_ij = P[P_idx];
+            double Q_ij = Qsum_inv * y_dist_p1_inv;
             double PQ_diff = P_ij - Q_ij;
             double PQ_diff_times_p1_inv = PQ_diff*y_dist_p1_inv;
 
-            dY[r] += y_diff0 * PQ_diff_times_p1_inv;
-            dY[r + 1] += y_diff1 * PQ_diff_times_p1_inv;
+            double d1 = y_diff0 * PQ_diff_times_p1_inv;
+            double d2 = y_diff1 * PQ_diff_times_p1_inv;
+
+            dYi0 += d1;
+            dYi1 += d2;
+
+            dY[2*j + 0] -= d1;
+            dY[2*j + 1] -= d2;
+
+            P_idx++;
         }
+
+        dY[2*i + 0] = dYi0;
+        dY[2*i + 1] = dYi1;
+
     }
 }
 
@@ -198,17 +205,15 @@ static void update_gains(double *gains, double *iY, double *dY, int n, int dy)
 {
     for (int i = 0; i < n; i++)
     {
-        int q = i * dy;
         for (int k = 0; k < dy; k++)
         {
-            int w = q + k;
-            if ((dY[w] > 0.0) != (iY[w] > 0.0))
+            if ((dY[i * dy + k] > 0.0) != (iY[i * dy + k] > 0.0))
             {
-                gains[w] += 0.2;
+                gains[i * dy + k] += 0.2;
             }
             else
             {
-                gains[w] *= 0.8;
+                gains[i * dy + k] *= 0.8;
             }
         }
     }
@@ -222,13 +227,10 @@ static void update(double *Y, double *iY, double *dY, double *gains, int iter, i
 
     for (int i = 0; i < n; i++)
     {
-        int q = i * dy;
         for (int k = 0; k < dy; k++)
         {
-            int w = q + k;
-            double a = iY[w] * momentum;
-            iY[w] = a - eta * gains[w] * dY[w];
-            Y[w] += iY[w];
+            iY[i * dy + k] = iY[i * dy + k] * momentum - eta * gains[i * dy + k] * dY[i * dy + k];
+            Y[i * dy + k] += iY[i * dy + k];
         }
     }
 }
@@ -237,24 +239,26 @@ static void update(double *Y, double *iY, double *dY, double *gains, int iter, i
 // W(n,dim_y) = 2*n*dim_y
 static void center_y(double *Y, int n, int dy)
 {
-    double *Y_SUM = (double*)malloc(sizeof(double) * dy);
+    double *Y_SUM = (double*) malloc (sizeof(double) * n * dy);
+
     for (int i = 0; i < dy; i++)
     {
-        double sum = 0.0;
+        Y_SUM[i] = 0.0;
         for (int j = 0; j < n; j++)
         {
-            int q = j * dy;
-            sum += Y[q + i];
+            Y_SUM[i] += Y[j*dy+i];
         }
-        Y_SUM[i] = sum / n;
     }
 
+    for (int k = 0; k < dy; k++)
+    {
+        Y_SUM[k] /= n;
+    }
     for (int i = 0; i < n; i++)
     {
-        int w = i * dy;
         for (int k = 0; k < dy; k++)
         {
-            Y[w + k] -= Y_SUM[k];
+            Y[i * dy + k] -= Y_SUM[k];
         }
     }
 
@@ -264,18 +268,11 @@ static void center_y(double *Y, int n, int dy)
 // Stops early exaggeration which divides all p's by 4.
 static void multiply_p(double *P, int n, double factor)
 {
-    int num_iters = n / 4;
-    __m256d factor_avx = _mm256_set1_pd(factor);
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++)
     {
-        int q = i * n;
-        for (int j = 0; j < num_iters; ++j)
+        for (int j = 0; j < n; j++)
         {
-            int w = j * 4;
-            int m = q + w;
-            __m256d P_avx = _mm256_loadu_pd(&P[m]);
-            P_avx = _mm256_mul_pd(P_avx, factor_avx);
-            _mm256_storeu_pd(&P[m], P_avx);
+            P[i * n + j] *= factor;
         }
     }
 }
@@ -284,7 +281,7 @@ uint64_t tsne_5_flops(tsne_data input, tsne_hyperparameters hyperparams) {
     uint64_t n = input.n;
     uint64_t iter = hyperparams.iterations;
 
-    return iter * 18*n*n;
+    return iter * 12*n*n;
 }
 
 void tsne_5(tsne_data input, tsne_hyperparameters hyperparams)
@@ -311,13 +308,11 @@ void tsne_5(tsne_data input, tsne_hyperparameters hyperparams)
     // Initialize some of the arrays
     for (int i = 0; i < n; i++)
     {
-        int q = i * dy;
         for (int k = 0; k < dy; k++)
         {
-            int w = q + k;
-            gains[w] = 1.0;
-            iY[w] = 0.0;
-            dY[w] = 0.0;
+            gains[i * dy + k] = 1.0;
+            iY[i * dy + k] = 0.0;
+            dY[i * dy + k] = 0.0;
         }
     }
 
@@ -332,9 +327,14 @@ void tsne_5(tsne_data input, tsne_hyperparameters hyperparams)
 
         // Update our state with the calculated gradients
         double momentum = 0.0;
-        // Set momentum based on the value of iter
-        momentum = hyperparams.initial_momentum + (hyperparams.final_momentum - hyperparams.initial_momentum) * (iter >= 20);
-
+        if (iter < 20)
+        {
+            momentum = hyperparams.initial_momentum;
+        }
+        else
+        {
+            momentum = hyperparams.final_momentum;
+        }
         update(Y, iY, dY, gains, iter, n, dy, hyperparams.eta, momentum);
 
         // Center y (mean of 0)
